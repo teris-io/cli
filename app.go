4326997b98a2416feebd477a9dfd8824904491b9
Oleg Sklyar <osklyar@gmx.com>
2017-05-26 15:26:05 +0200

    Splits parsing out of app and places app beyond an interface

--- app.go
+++ app.go
@@ -1,202 +1,76 @@
 package cli
 
 import (
-	"fmt"
 	"github.com/silvertern/cli/command"
 	"github.com/silvertern/cli/option"
-	"path"
-	"strconv"
-	"strings"
 )
 
-const (
-	helpKey = "help"
-	helpChar = 'h'
-	trueStr = "true"
-)
+type App interface {
+	Description() string
+	Args() []command.Arg
+	Options() []option.Option
+	Commands() []command.Command
+	Action() command.Action
 
-type App struct {
-	Description string
-	Args        []command.Arg
-	Options     []option.Option
-	Commands    []command.Command
-	Action      command.Action
+	WithArg(arg command.Arg) App
+	WithOption(opt option.Option) App
+	WithCommand(cmd command.Command) App
+	WithAction(action command.Action) App
+
+	Parse(appargs []string) (invocation []string, args []string, opts map[string]string, err error)
 }
 
-func (a *App) Parse(appargs []string) (invocation []string, args []string, opts map[string]string, err error) {
-	_, appname := path.Split(appargs[0])
-
-	invocation, argsAndOpts, expArgs, accptOpts := evalCommand(a, appargs[1:])
-	invocation = append([]string{appname}, invocation...)
-
-	if args, opts, err = splitArgsAndOpts(argsAndOpts, accptOpts); err == nil {
-		if _, ok := opts["help"]; !ok {
-			if err = assertArgs(expArgs, args); err == nil {
-				err = assertOpts(accptOpts, opts)
-			}
-		}
-	}
-	return invocation, args, opts, err
+func New(descr string) App {
+	return &app{descr: descr}
 }
 
-func evalCommand(a *App, appargs []string) (invocation []string, argsAndOpts []string, expArgs []command.Arg, accptOpts []option.Option) {
-	invocation = []string{}
-	argsAndOpts = appargs
-	expArgs = a.Args
-	accptOpts = a.Options
-
-	cmds2check := a.Commands
-	for i, arg := range appargs {
-		matched := false
-		for _, cmd := range cmds2check {
-			if cmd.Key() == arg || cmd.Shortcut() == arg {
-				invocation = append(invocation, cmd.Key())
-				argsAndOpts = appargs[i+1:]
-				expArgs = cmd.Args()
-				accptOpts = append(accptOpts, cmd.Options()...)
-
-				cmds2check = cmd.Commands()
-				matched = true
-				break
-			}
-		}
-		if !matched {
-			break
-		}
-	}
-	return invocation, argsAndOpts, expArgs, accptOpts
+type app struct {
+	descr  string
+	args   []command.Arg
+	opts   []option.Option
+	cmds   []command.Command
+	action command.Action
 }
 
-
-func splitArgsAndOpts(appargs []string, accptOpts []option.Option) (args []string, opts map[string]string, err error) {
-	opts = make(map[string]string)
-
-	danglingOpt := ""
-	for _, arg := range appargs {
-		if danglingOpt != "" {
-			opts[danglingOpt] = arg
-			danglingOpt = ""
-			continue
-		}
-
-		if strings.HasPrefix(arg, "--") {
-			arg = arg[2:]
-			if arg == helpKey {
-				return nil, map[string]string{helpKey: trueStr}, nil
-			}
-			parts := strings.Split(arg, "=")
-			key := parts[0]
-			matched := false
-			for _, accptOpt := range accptOpts {
-				if accptOpt.Key() == key {
-					if accptOpt.Type() == option.TypeBool {
-						if len(parts) == 1 {
-							opts[accptOpt.Key()] = trueStr
-						} else {
-							return args, opts, fmt.Errorf("boolean options have true assigned implicitly, found value for --%s", key)
-						}
-					} else if len(parts) >= 2 {
-						opts[accptOpt.Key()] = strings.Join(parts[1:], "=") // permit = in values
-					} else {
-						return args, opts, fmt.Errorf("missing value for option --%s", key)
-					}
-					matched = true
-					break
-				}
-			}
-			if ! matched {
-				return args, opts, fmt.Errorf("unknown option --%s", key)
-			}
-			continue
-		}
-
-		if strings.HasPrefix(arg, "-") {
-			arg = arg[1:]
-
-			for i, char := range arg {
-				if char == helpChar {
-					return nil, map[string]string{helpKey: trueStr}, nil
-				}
-				matched := false
-				for _, accptOpt := range accptOpts {
-					if accptOpt.CharKey() == char {
-						if accptOpt.Type() == option.TypeBool {
-							opts[accptOpt.Key()] = trueStr
-						} else if i == len(arg)-1 {
-							danglingOpt = accptOpt.Key()
-						} else {
-							return args, opts, fmt.Errorf("non-boolean flag -%v in non-terminal position", string(char))
-						}
-						matched = true
-						break
-					}
-				}
-				if !matched {
-					return args, opts, fmt.Errorf("unknown flag -%v", string(char))
-				}
-			}
-			continue
-		}
-
-		args = append(args, arg)
-	}
-	if danglingOpt != "" {
-		return args, opts, fmt.Errorf("dangling option --%s", danglingOpt)
-	}
-	return args, opts, nil
+func (a *app) Description() string {
+	return a.descr
 }
 
-func assertArgs(expected []command.Arg, actual []string) error {
-	if len(expected) == 0 || !expected[len(expected) - 1].Optional {
-		if len(expected) > len(actual) {
-			return fmt.Errorf("missing required argument %v", expected[len(actual)].Key)
-		}	else if len(expected) < len(actual) {
-			return fmt.Errorf("unknown arguments %v", actual[len(expected):])
-		}
-	}
-	for i, e := range expected {
-		if len(actual) < i+1 {
-			if !e.Optional {
-				return fmt.Errorf("missing required argument %s", e.Key)
-			}
-			break
-		}
-		arg := actual[i]
-		switch e.Type {
-		case option.TypeBool:
-			if _, err := strconv.ParseBool(arg); err != nil {
-				return fmt.Errorf("argument %s must be a boolean value, found %v", e.Key, arg)
-			}
-		case option.TypeInt:
-			if _, err := strconv.ParseInt(arg, 10, 64); err != nil {
-				return fmt.Errorf("argument %s must be an integer value, found %v", e.Key, arg)
-			}
-		case option.TypeNumber:
-			if _, err := strconv.ParseFloat(arg, 64); err != nil {
-				return fmt.Errorf("argument %s must be a number, found %v", e.Key, arg)
-			}
-		}
-	}
-	return nil
+func (a *app) Args() []command.Arg {
+	return a.args
 }
 
-func assertOpts(permitted []option.Option, actual map[string]string) error {
-	for key, value := range actual {
-		for _, p := range permitted {
-			if p.Key() == key {
-				switch p.Type() {
-				case option.TypeInt:
-					if _, err := strconv.ParseInt(value, 10, 64); err != nil {
-						return fmt.Errorf("option --%s must be given an integer value, found %v", p.Key(), value)
-					}
-				case option.TypeNumber:
-					if _, err := strconv.ParseFloat(value, 64); err != nil {
-						return fmt.Errorf("option --%s must must be given a number, found %v", p.Key(), value)
-					}
-				}
-				break
-			}
-		}
-	}
-	return nil
+func (a *app) Options() []option.Option {
+	return a.opts
+}
+
+func (a *app) Commands() []command.Command {
+	return a.cmds
+}
+
+func (a *app) Action() command.Action {
+	return a.action
+}
+
+func (a *app) WithArg(arg command.Arg) App {
+	a.args = append(a.args, arg)
+	return a
+}
+
+func (a *app) WithOption(opt option.Option) App {
+	a.opts = append(a.opts, opt)
+	return a
+}
+
+func (a *app) WithCommand(cmd command.Command) App {
+	a.cmds = append(a.cmds, cmd)
+	return a
+}
+func (a *app) WithAction(action command.Action) App {
+	a.action = action
+	return a
+}
+
+func (a *app) Parse(appargs []string) (invocation []string, args []string, opts map[string]string, err error) {
+	return Parse(a, appargs)
 }
--- parse_test.go
+++ parse_test.go
@@ -9,7 +9,7 @@ import (
 	"testing"
 )
 
-func setup() *cli.App {
+func setup_parse_app() cli.App {
 	co := command.New("checkout", "checkout a branch or revision").
 		WithShortcut("co").
 		WithArg(command.Arg{Key: "branch"}).
@@ -32,206 +32,202 @@ func setup() *cli.App {
 
 	rmt := command.New("remote", "operations with remotes").WithCommand(add)
 
-	return &cli.App{
-		Args: []command.Arg{
-			command.Arg{Key: "arg1"},
-		},
-		Commands: []command.Command{
-			co, rmt,
-		},
-	}
+	return cli.New("git tool").
+		WithArg(command.Arg{Key: "arg1"}).
+		WithCommand(co).
+		WithCommand(rmt)
 }
 
 func TestApp_Parse_DropsPathFromAppName_Ok(t *testing.T) {
-	invocation, args, opts, err := setup().Parse([]string{"~/some/path/git", "checkout", "dev"})
+	invocation, args, opts, err := cli.Parse(setup_parse_app(), []string{"~/some/path/git", "checkout", "dev"})
 	assertAppParseOk(t, "[git checkout] [dev] map[]", invocation, args, opts, err)
 }
 
 func TestApp_Parse_DropsDotPathFromAppName_Ok(t *testing.T) {
-	invocation, args, opts, err := setup().Parse([]string{"./git", "checkout", "dev"})
+	invocation, args, opts, err := cli.Parse(setup_parse_app(), []string{"./git", "checkout", "dev"})
 	assertAppParseOk(t, "[git checkout] [dev] map[]", invocation, args, opts, err)
 }
 
 func TestApp_Parse_NoFlags_Ok(t *testing.T) {
-	invocation, args, opts, err := setup().Parse([]string{"git", "checkout", "dev"})
+	invocation, args, opts, err := cli.Parse(setup_parse_app(), []string{"git", "checkout", "dev"})
 	assertAppParseOk(t, "[git checkout] [dev] map[]", invocation, args, opts, err)
 }
 
 func TestApp_Parse_1xCharBoolFlag_Ok(t *testing.T) {
-	invocation, args, opts, err := setup().Parse([]string{"git", "checkout", "-b", "dev"})
+	invocation, args, opts, err := cli.Parse(setup_parse_app(), []string{"git", "checkout", "-b", "dev"})
 	assertAppParseOk(t, "[git checkout] [dev] map[branch:true]", invocation, args, opts, err)
 }
 
 func TestApp_Parse_2xCharBoolFlags_Ok(t *testing.T) {
-	invocation, args, opts, err := setup().Parse([]string{"git", "checkout", "-b", "-u", "dev"})
+	invocation, args, opts, err := cli.Parse(setup_parse_app(), []string{"git", "checkout", "-b", "-u", "dev"})
 	assertAppParseOk(t, "[git checkout] [dev] map[branch:true upstream:true]", invocation, args, opts, err)
 }
 
 func TestApp_Parse_2xCharBoolFlagsAsOne_Ok(t *testing.T) {
-	invocation, args, opts, err := setup().Parse([]string{"git", "checkout", "-bu", "dev"})
+	invocation, args, opts, err := cli.Parse(setup_parse_app(), []string{"git", "checkout", "-bu", "dev"})
 	assertAppParseOk(t, "[git checkout] [dev] map[branch:true upstream:true]", invocation, args, opts, err)
 }
 
 func TestApp_Parse_MultiCharStringLast_Ok(t *testing.T) {
-	invocation, args, opts, err := setup().Parse([]string{"git", "checkout", "-buf", "master", "dev"})
+	invocation, args, opts, err := cli.Parse(setup_parse_app(), []string{"git", "checkout", "-buf", "master", "dev"})
 	assertAppParseOk(t, "[git checkout] [dev] map[branch:true fallback:master upstream:true]", invocation, args, opts, err)
 }
 
 func TestApp_Parse_MultiCharIntLast_Ok(t *testing.T) {
-	invocation, args, opts, err := setup().Parse([]string{"git", "checkout", "-buc", "1", "dev"})
+	invocation, args, opts, err := cli.Parse(setup_parse_app(), []string{"git", "checkout", "-buc", "1", "dev"})
 	assertAppParseOk(t, "[git checkout] [dev] map[branch:true count:1 upstream:true]", invocation, args, opts, err)
 }
 
 func TestApp_Parse_MultiCharNumberLast_Ok(t *testing.T) {
-	invocation, args, opts, err := setup().Parse([]string{"git", "checkout", "-bup", "3.14", "dev"})
+	invocation, args, opts, err := cli.Parse(setup_parse_app(), []string{"git", "checkout", "-bup", "3.14", "dev"})
 	assertAppParseOk(t, "[git checkout] [dev] map[branch:true pi:3.14 upstream:true]", invocation, args, opts, err)
 }
 
 func TestApp_Parse_1xBoolFlag_Ok(t *testing.T) {
-	invocation, args, opts, err := setup().Parse([]string{"git", "checkout", "--branch", "dev"})
+	invocation, args, opts, err := cli.Parse(setup_parse_app(), []string{"git", "checkout", "--branch", "dev"})
 	assertAppParseOk(t, "[git checkout] [dev] map[branch:true]", invocation, args, opts, err)
 }
 
 func TestApp_Parse_2xBoolFlag_Ok(t *testing.T) {
-	invocation, args, opts, err := setup().Parse([]string{"git", "checkout", "--branch", "--upstream", "dev"})
+	invocation, args, opts, err := cli.Parse(setup_parse_app(), []string{"git", "checkout", "--branch", "--upstream", "dev"})
 	assertAppParseOk(t, "[git checkout] [dev] map[branch:true upstream:true]", invocation, args, opts, err)
 }
 
 func TestApp_Parse_2xBoolAnd1xStringFlag_Ok(t *testing.T) {
-	invocation, args, opts, err := setup().Parse([]string{"git", "checkout", "--fallback=master", "--branch", "--upstream", "dev"})
+	invocation, args, opts, err := cli.Parse(setup_parse_app(), []string{"git", "checkout", "--fallback=master", "--branch", "--upstream", "dev"})
 	assertAppParseOk(t, "[git checkout] [dev] map[branch:true fallback:master upstream:true]", invocation, args, opts, err)
 }
 
 func TestApp_Parse_RedundantFlags_Ok(t *testing.T) {
-	invocation, args, opts, err := setup().Parse([]string{"git", "checkout", "-b", "--branch", "dev"})
+	invocation, args, opts, err := cli.Parse(setup_parse_app(), []string{"git", "checkout", "-b", "--branch", "dev"})
 	assertAppParseOk(t, "[git checkout] [dev] map[branch:true]", invocation, args, opts, err)
 }
 
 func TestApp_Parse_NestedCommandWithFlags_Ok(t *testing.T) {
-	invocation, args, opts, err := setup().Parse([]string{"git", "remote", "add", "origin", "-f", "1", "3.14", "true"})
+	invocation, args, opts, err := cli.Parse(setup_parse_app(), []string{"git", "remote", "add", "origin", "-f", "1", "3.14", "true"})
 	assertAppParseOk(t, "[git remote add] [origin 1 3.14 true] map[force:true]", invocation, args, opts, err)
 }
 
 func TestApp_Parse_OptionalMissing_Ok(t *testing.T) {
-	invocation, args, opts, err := setup().Parse([]string{"git", "remote", "add", "origin", "1", "3.14", "true"})
+	invocation, args, opts, err := cli.Parse(setup_parse_app(), []string{"git", "remote", "add", "origin", "1", "3.14", "true"})
 	assertAppParseOk(t, "[git remote add] [origin 1 3.14 true] map[]", invocation, args, opts, err)
 }
 
 func TestApp_Parse_OptionalPresent_Ok(t *testing.T) {
-	invocation, args, opts, err := setup().Parse([]string{"git", "remote", "add", "origin", "1", "3.14", "true", "true"})
+	invocation, args, opts, err := cli.Parse(setup_parse_app(), []string{"git", "remote", "add", "origin", "1", "3.14", "true", "true"})
 	assertAppParseOk(t, "[git remote add] [origin 1 3.14 true true] map[]", invocation, args, opts, err)
 }
 
 func TestApp_Parse_KeysAnywhereBetweenArgs_Ok(t *testing.T) {
-	invocation, args, opts, err := setup().Parse([]string{"git", "remote", "add", "-f", "origin", "--default=foo", "1", "3.14", "true", "-q"})
+	invocation, args, opts, err := cli.Parse(setup_parse_app(), []string{"git", "remote", "add", "-f", "origin", "--default=foo", "1", "3.14", "true", "-q"})
 	assertAppParseOk(t, "[git remote add] [origin 1 3.14 true] map[default:foo force:true quiet:true]", invocation, args, opts, err)
 }
 
 func TestApp_Parse_ExplicitValueForBoolOption_Error(t *testing.T) {
-	invocation, args, opts, err := setup().Parse([]string{"git", "remote", "add", "--force=true", "origin", "1", "3.14", "true"})
+	invocation, args, opts, err := cli.Parse(setup_parse_app(), []string{"git", "remote", "add", "--force=true", "origin", "1", "3.14", "true"})
 	assertAppParseError(t, "[git remote add] [] map[]",
 		"boolean options have true assigned implicitly, found value for --force", invocation, args, opts, err)
 }
 
 func TestApp_Parse_EqSignInStringOptionValue_Ok(t *testing.T) {
-	invocation, args, opts, err := setup().Parse([]string{"git", "remote", "add", "--default=foo=boo=blah", "origin", "1", "3.14", "true"})
+	invocation, args, opts, err := cli.Parse(setup_parse_app(), []string{"git", "remote", "add", "--default=foo=boo=blah", "origin", "1", "3.14", "true"})
 	assertAppParseOk(t, "[git remote add] [origin 1 3.14 true] map[default:foo=boo=blah]", invocation, args, opts, err)
 }
 
 func TestApp_Parse_UnrecognizedCommand_ErrorUnknownFlagForRoot(t *testing.T) {
-	invocation, args, opts, err := setup().Parse([]string{"git", "foo", "-f", "origin"})
+	invocation, args, opts, err := cli.Parse(setup_parse_app(), []string{"git", "foo", "-f", "origin"})
 	assertAppParseError(t, "[git] [foo] map[]", "unknown flag -f", invocation, args, opts, err)
 }
 
 func TestApp_Parse_UnrecognizedCommand_ErrorUnknownArgument(t *testing.T) {
-	invocation, args, opts, err := setup().Parse([]string{"git", "foo", "origin"})
+	invocation, args, opts, err := cli.Parse(setup_parse_app(), []string{"git", "foo", "origin"})
 	assertAppParseError(t, "[git] [foo origin] map[]", "unknown arguments [origin]", invocation, args, opts, err)
 }
 
 func TestApp_Parse_BaseApp_ErrorMissingArgument(t *testing.T) {
-	invocation, args, opts, err := setup().Parse([]string{"git"})
+	invocation, args, opts, err := cli.Parse(setup_parse_app(), []string{"git"})
 	assertAppParseError(t, "[git] [] map[]", "missing required argument arg1", invocation, args, opts, err)
 }
 
 func TestApp_Parse_DanglingOptions_Error(t *testing.T) {
-	invocation, args, opts, err := setup().Parse([]string{"git", "checkout", "dev", "-p"})
+	invocation, args, opts, err := cli.Parse(setup_parse_app(), []string{"git", "checkout", "dev", "-p"})
 	assertAppParseError(t, "[git checkout] [dev] map[]", "dangling option --pi", invocation, args, opts, err)
 }
 
 func TestApp_Parse_LastArgOptionalRequiredMissing_Error(t *testing.T) {
-	invocation, args, opts, err := setup().Parse([]string{"git", "remote", "add", "origin", "1"})
+	invocation, args, opts, err := cli.Parse(setup_parse_app(), []string{"git", "remote", "add", "origin", "1"})
 	assertAppParseError(t, "[git remote add] [origin 1] map[]", "missing required argument pi", invocation, args, opts, err)
 }
 
 func TestApp_Parse_IncorrectBoolArgType_Error(t *testing.T) {
-	invocation, args, opts, err := setup().Parse([]string{"git", "remote", "add", "origin", "1", "3.14", "foo"})
+	invocation, args, opts, err := cli.Parse(setup_parse_app(), []string{"git", "remote", "add", "origin", "1", "3.14", "foo"})
 	assertAppParseError(t, "[git remote add] [origin 1 3.14 foo] map[]", "argument force must be a boolean value, found foo", invocation, args, opts, err)
 }
 
 func TestApp_Parse_IncorrectIntArgType_Error(t *testing.T) {
-	invocation, args, opts, err := setup().Parse([]string{"git", "remote", "add", "origin", "3.14", "3.14", "true"})
+	invocation, args, opts, err := cli.Parse(setup_parse_app(), []string{"git", "remote", "add", "origin", "3.14", "3.14", "true"})
 	assertAppParseError(t, "[git remote add] [origin 3.14 3.14 true] map[]", "argument count must be an integer value, found 3.14", invocation, args, opts, err)
 }
 
 func TestApp_Parse_IncorrectNumberArgType_Error(t *testing.T) {
-	invocation, args, opts, err := setup().Parse([]string{"git", "remote", "add", "origin", "1", "aaa", "true"})
+	invocation, args, opts, err := cli.Parse(setup_parse_app(), []string{"git", "remote", "add", "origin", "1", "aaa", "true"})
 	assertAppParseError(t, "[git remote add] [origin 1 aaa true] map[]", "argument pi must be a number, found aaa", invocation, args, opts, err)
 }
 
 func TestApp_Parse_IncorrectOptionalArgType_Error(t *testing.T) {
-	invocation, args, opts, err := setup().Parse([]string{"git", "remote", "add", "origin", "1", "3.14", "true", "25"})
+	invocation, args, opts, err := cli.Parse(setup_parse_app(), []string{"git", "remote", "add", "origin", "1", "3.14", "true", "25"})
 	assertAppParseError(t, "[git remote add] [origin 1 3.14 true 25] map[]", "argument optional must be a boolean value, found 25", invocation, args, opts, err)
 }
 
 func TestApp_Parse_NonBooleanFlagInNonTerminalPosition_Error(t *testing.T) {
-	invocation, args, opts, err := setup().Parse([]string{"git", "checkout", "-bpu", "3.14", "dev"})
+	invocation, args, opts, err := cli.Parse(setup_parse_app(), []string{"git", "checkout", "-bpu", "3.14", "dev"})
 	assertAppParseError(t, "[git checkout] [] map[branch:true]", "non-boolean flag -p in non-terminal position", invocation, args, opts, err)
 }
 
 func TestApp_Parse_MissingValueForOption_Error(t *testing.T) {
-	invocation, args, opts, err := setup().Parse([]string{"git", "checkout", "--pi", "dev"})
+	invocation, args, opts, err := cli.Parse(setup_parse_app(), []string{"git", "checkout", "--pi", "dev"})
 	assertAppParseError(t, "[git checkout] [] map[]", "missing value for option --pi", invocation, args, opts, err)
 }
 
 func TestApp_Parse_NoValueAfterTheEqualSignForStringOption_Ok(t *testing.T) {
-	invocation, args, opts, err := setup().Parse([]string{"git", "checkout", "--str=", "dev"})
+	invocation, args, opts, err := cli.Parse(setup_parse_app(), []string{"git", "checkout", "--str=", "dev"})
 	assertAppParseOk(t, "[git checkout] [dev] map[str:]", invocation, args, opts, err)
 }
 
 func TestApp_Parse_UnknownOption_Error(t *testing.T) {
-	invocation, args, opts, err := setup().Parse([]string{"git", "checkout", "--foo=25", "dev"})
+	invocation, args, opts, err := cli.Parse(setup_parse_app(), []string{"git", "checkout", "--foo=25", "dev"})
 	assertAppParseError(t, "[git checkout] [] map[]", "unknown option --foo", invocation, args, opts, err)
 }
 
 func TestApp_Parse_IncorrectDataForIntOption_Error(t *testing.T) {
-	invocation, args, opts, err := setup().Parse([]string{"git", "checkout", "-c", "2.25", "dev"})
+	invocation, args, opts, err := cli.Parse(setup_parse_app(), []string{"git", "checkout", "-c", "2.25", "dev"})
 	assertAppParseError(t, "[git checkout] [dev] map[count:2.25]", "option --count must be given an integer value, found 2.25", invocation, args, opts, err)
 }
 
 func TestApp_Parse_IncorrectDataForNumberOption_Error(t *testing.T) {
-	invocation, args, opts, err := setup().Parse([]string{"git", "checkout", "-p", "aaa", "dev"})
+	invocation, args, opts, err := cli.Parse(setup_parse_app(), []string{"git", "checkout", "-p", "aaa", "dev"})
 	assertAppParseError(t, "[git checkout] [dev] map[pi:aaa]", "option --pi must must be given a number, found aaa", invocation, args, opts, err)
 }
 
 func TestApp_Parse_LastArgOptionalPermitsUnlimitedExtraArgs_Error(t *testing.T) {
-	invocation, args, opts, err := setup().Parse([]string{"git", "remote", "add", "origin", "1", "3.14", "true", "true", "exra1", "extra2"})
+	invocation, args, opts, err := cli.Parse(setup_parse_app(), []string{"git", "remote", "add", "origin", "1", "3.14", "true", "true", "exra1", "extra2"})
 	assertAppParseOk(t, "[git remote add] [origin 1 3.14 true true exra1 extra2] map[]", invocation, args, opts, err)
 }
 
 func TestApp_Parse_HelpOptionComesOutWithoutArgOrFlagValidation_Ok(t *testing.T) {
-	invocation, args, opts, err := setup().Parse([]string{"git", "checkout", "-c", "3.15", "dev", "arg2", "arg3", "--help"})
+	invocation, args, opts, err := cli.Parse(setup_parse_app(), []string{"git", "checkout", "-c", "3.15", "dev", "arg2", "arg3", "--help"})
 	assertAppParseOk(t, "[git checkout] [] map[help:true]", invocation, args, opts, err)
 }
 
 func TestApp_Parse_HelpFlagInMultichar_Ok(t *testing.T) {
-	invocation, args, opts, err := setup().Parse([]string{"git", "checkout", "-bhc", "3.15", "dev"})
+	invocation, args, opts, err := cli.Parse(setup_parse_app(), []string{"git", "checkout", "-bhc", "3.15", "dev"})
 	assertAppParseOk(t, "[git checkout] [] map[help:true]", invocation, args, opts, err)
 }
 
 func assertAppParseOk(t *testing.T, expected string, invocation []string, args []string, opts map[string]string, err error) {
 	if err == nil {
 		optkeys := []string{}
-		for key, _ := range opts {
+		for key := range opts {
 			optkeys = append(optkeys, key)
 		}
 		sort.Strings(optkeys)
@@ -249,7 +245,7 @@ func assertAppParseOk(t *testing.T, expected string, invocation []string, args [
 
 func assertAppParseError(t *testing.T, expectedData, expectedError string, invocation []string, args []string, opts map[string]string, err error) {
 	optkeys := []string{}
-	for key, _ := range opts {
+	for key := range opts {
 		optkeys = append(optkeys, key)
 	}
 	sort.Strings(optkeys)
